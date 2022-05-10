97c69f859be8f2e6db75bacb719344cb03c359ab
Dagart <65997824+dagartga@users.noreply.github.com>
2022-05-09 17:10:54 -0700

    Update lambda_handler.py

--- lambda_handler.py
+++ lambda_handler.py
@@ -9,12 +9,13 @@ def lambda_handler(event, context):
 
     import boto3
     import botocore
-
-
+    
+    ACCESS_KEY_ID = "XXXXXX"
+    ACCESS_SECRET_KEY = "XXXXXXX"
 
     s3 = boto3.client('s3',
-                      aws_access_key_id="XXXXXXX",
-                      aws_secret_access_key="XXXXXXXXX")
+                      aws_access_key_id=ACCESS_KEY_ID,
+                      aws_secret_access_key=ACCESS_SECRET_KEY)
 
     # get the raw data for the features from bucket
     raw_data_obj = s3.get_object(
@@ -29,36 +30,17 @@ def lambda_handler(event, context):
     price_data_obj = s3.get_object(Bucket='btcpricedata', Key='price_df.csv')
     price_df = pd.read_csv(price_data_obj['Body'])
 
-    s3 = boto3.resource('s3')
-    BUCKET_NAME = 'btcpricedata'
-    KEY = 'scaler.save'
-    try:
-        local_file_name = '/tmp/' + KEY
-        s3.Bucket(BUCKET_NAME).download_file(KEY, local_file_name)
-    except botocore.exceptions.ClientError as e:
-        if e.response['Error']['Code'] != "404":
-            raise Exception('Error loading scaler.save into tmp directory')
-
     # get the scaler file
-    scale = joblib.load('./tmp/scaler.save')
+    scale = joblib.load('./scaler.save')
 
     # scale the new data and set type and shape
     X = scale.transform(last_row.iloc[:, 1:])
     X = X.astype('float32')
     X = np.reshape(X, (1, 9))
 
-    # load the tensorflow lite model into lambda function memory
-    KEY = 'btc_nextday_v052022.tflite'
-    try:
-        local_file_name = '/tmp/' + KEY
-        s3.Bucket(BUCKET_NAME).download_file(KEY, local_file_name)
-    except botocore.exceptions.ClientError as e:
-        if e.response['Error']['Code'] != "404":
-            raise Exception(
-                'Error loading btc_nextday_v052022.tflite into tmp directory')
 
     # call the tensorflow lite function
-    model_local_path = './tmp/btc_nextday_v052022.tflite'
+    model_local_path = './btc_nextday_v052022.tflite'
 
     interpreter = tflite.Interpreter(model_path=model_local_path)
     interpreter.allocate_tensors()
@@ -75,7 +57,7 @@ def lambda_handler(event, context):
     preds = interpreter.get_tensor(output_index)
 
     # add prediction to price_df
-    price_df.loc[-1, 'next_day_pred'] = preds[0]
+    price_df.iloc[-1, 3] = preds[0]
     # update the next day prices
     price_df['next_day_price'] = price_df['price'].shift(-1)
 
